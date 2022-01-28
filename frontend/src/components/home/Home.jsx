3b124f770f295431a4c29b682834ad4ccff433ae
unknown <luciamilorodg@gmail.com>
2022-01-28 16:17:19 -0300

    categorias de la DB iconos del scrollnav

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import HeaderLogo from './HeaderLogo';
-import NavCategory from './NavCategory'
-import CardList from './CardList';
-import BottomIconNav from './BottomIconNav';
-import HeaderWsp from './HeaderWsp';
-import HomeFooter from './HomeFooter';
-
+import React from "react";
+import HeaderLogo from "./HeaderLogo";
+import NavCategory from "./NavCategory";
+import CardList from "./CardList";
+import BottomIconNav from "./BottomIconNav";
+import HeaderWsp from "./HeaderWsp";
+import HomeFooter from "./HomeFooter";
 
 function Home() {
-    return (
-        <div className="home__container">
-             <HeaderLogo />
-             <NavCategory />
-             <CardList />
-             <BottomIconNav />
-             <HeaderWsp />
-             <HomeFooter />
-        </div>
-    )
+  return (
+    <div className="home__container">
+      <header>
+        <HeaderLogo />
+        <HeaderWsp />
+        <NavCategory />
+      </header>
+      <main>
+        <CardList />
+        <BottomIconNav />
+      </main>
+      <footer>
+        <HomeFooter />
+      </footer>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
