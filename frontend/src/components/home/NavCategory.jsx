aed764bcb96b926f8154fc5d3406eef0ad0bc568
unknown <luciamilorodg@gmail.com>
2022-01-28 13:30:35 -0300

    categorias de la DB

--- frontend/src/components/home/NavCategory.jsx
+++ frontend/src/components/home/NavCategory.jsx
@@ -9,6 +9,11 @@ import { BiDrink } from "react-icons/bi";
 import { GiFlowerPot } from "react-icons/gi";
 import { GiBookshelf } from "react-icons/gi";
 import { GiDesk } from "react-icons/gi";
+import { GiCandleLight } from "react-icons/gi";
+import { BsFillDoorOpenFill } from "react-icons/bs";
+import { BsFillBasketFill } from "react-icons/bs";
+import { GiRunningShoe} from "react-icons/gi";
+import { GrCoatCheck} from "react-icons/gr";
 
 function NavCategory() {
   return (
@@ -19,35 +24,19 @@ function NavCategory() {
         </div>
         <span className={styles.nav__text}>Iluminación</span>
       </a>
+
       <a href="#" className={styles.topnav__link}>
         <div className={styles.topnav__icon}>
           <GiDesk />
         </div>
         <span className={styles.nav__text}>Escritorios</span>
       </a>
-      <a href="#" className={styles.topnav__link}>
-        <div className={styles.topnav__icon}>
-          <BiBed />
-        </div>
-        <span className={styles.nav__text}>Almohadones</span>
-      </a>
-      <a href="#" className={styles.topnav__link}>
-        <div className={styles.topnav__icon}>
-          <GiMirrorMirror />
-        </div>
-        <span className={styles.nav__text}>Espejos</span>
-      </a>
+
       <a href="#" className={styles.topnav__link}>
         <div className={styles.topnav__icon}>
           <GiTable />
         </div>
         <span className={styles.nav__text}>Mesas</span>
-      </a>
-      <a href="#" className={styles.topnav__link}>
-        <div className={styles.topnav__icon}>
-          <GiSofa />
-        </div>
-        <span className={styles.nav__text}>Asientos</span>
       </a>
 
       <a href="#" className={styles.topnav__link}>
@@ -56,18 +45,96 @@ function NavCategory() {
         </div>
         <span className={styles.nav__text}>Bares</span>
       </a>
-      <a href="#" className={styles.topnav__link}>
-        <div className={styles.topnav__icon}>
-          <GiFlowerPot />
-        </div>
-        <span className={styles.nav__text}>Macetas</span>
-      </a>
+
       <a href="#" className={styles.topnav__link}>
         <div className={styles.topnav__icon}>
           <GiBookshelf />
         </div>
         <span className={styles.nav__text}>Estanterías</span>
       </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <BsFillDoorOpenFill />
+        </div>
+        <span className={styles.nav__text}>Recibidores</span>
+      </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <GiFlowerPot />
+        </div>
+        <span className={styles.nav__text}>Macetas</span>
+      </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <GiMirrorMirror />
+        </div>
+        <span className={styles.nav__text}>Espejos</span>
+      </a>
+
+
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <GrCoatCheck />
+        </div>
+        <span className={styles.nav__text}>Percheros</span>
+      </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <BiBed />
+        </div>
+        <span className={styles.nav__text}>Almohadones</span>
+      </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <GiRunningShoe />
+        </div>
+        <span className={styles.nav__text}>Zapateros</span>
+      </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <BiBed />
+        </div>
+        <span className={styles.nav__text}>Almohadones</span>
+      </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <BsFillBasketFill  />
+        </div>
+        <span className={styles.nav__text}>Canastos</span>
+      </a>
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <GiCandleLight />
+        </div>
+        <span className={styles.nav__text}>Velas</span>
+      </a>
+
+
+
+
+
+
+
+      <a href="#" className={styles.topnav__link}>
+        <div className={styles.topnav__icon}>
+          <GiSofa />
+        </div>
+        <span className={styles.nav__text}>Asientos</span>
+      </a>
+
+
+
+
+
 
       {/* <div className={styles.item}>
         <BiBed />
