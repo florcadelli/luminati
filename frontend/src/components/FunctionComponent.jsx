32cee00bed5bc3fbd903c5cf2cb5da801b6b86be
florcadelli <florcadelli@gmail.com>
2022-01-28 13:40:51 -0300

    carpeta backend

--- frontend/src/components/FunctionComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FunctionComponent = ({props}) => {
-return (
-<li>{props.nombre} traerá {props.tarea}</li>
-)
-}
-
-export default FunctionComponent
--- frontend/src/components/forms/HeaderForms.jsx
+++ frontend/src/components/forms/HeaderForms.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import HeaderLogo from '../home/HeaderLogo.jsx';
 import styles from "../../styles/forms-styles/header-forms.module.css";
 import { Link } from "react-router-dom";
 
+
 const HeaderForms = ({ props }) => {
   return (
-    <div className={styles.header}>
+    <div>
+        <div className={styles.header}>
       <div className={styles.line}></div>
       <Link className={styles.logo}  to="/">
         <img src={require("../../img/logo-luminati.png")} alt="Logo" />
       </Link>
       <div className={styles.line}></div>
+     </div>
+     <div className={styles.desktop}>
+        <HeaderLogo/>
+     </div>
+     
     </div>
+    
   );
 };
 
