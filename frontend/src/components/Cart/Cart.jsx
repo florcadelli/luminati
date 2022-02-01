519d1d47437836ced6394a4978e4fb40009dfbfd
sandra0411 <sandrapaludivan@gmail.com>
2022-02-01 18:40:18 -0300

    Carrito Responsive

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -2,45 +2,59 @@ import ProductInCart from "./ProductInCart";
 import styles from './../../styles/cart.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import HeaderLogo from './../home/HeaderLogo'
+import HomeFooter from './../home/HomeFooter'
 
 const Cart = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.cartHeader}>
-                <div className={styles.divBackIcon}>
-                    <Link to="/luminati"><FontAwesomeIcon icon={faChevronLeft} className={styles.backIcon} /></Link>
-                    
+
+        <>
+            <div className={styles.versionDesk}> <HeaderLogo /> </div>
+
+            <div className={styles.container}>
+                <div className={styles.cartHeader}>
+                    <div className={styles.divBackIcon}>
+                        <Link to="/luminati"><FontAwesomeIcon icon={faChevronLeft} className={styles.backIcon} /></Link>
+
+                    </div>
+                    <p>Mi Carrito</p>
+                    <p></p>
                 </div>
-                <p>Mi Carrito</p>
-                <p></p>
+
+                <div className={styles.containerDesk}>
+
+                    <main className={styles.mainCart}>
+
+                        <ProductInCart />
+                        <ProductInCart />
+                        <ProductInCart />
+                        <ProductInCart />
+                        <ProductInCart />
+                                                
+                    </main>
+
+                    <footer className={styles.footerCart}>
+                        <div className={styles.line}></div>
+
+                        <div className={styles.totalPrice}>
+                            <p>Total</p>
+                            <p>$ 30.000</p>
+                        </div>
+
+                        <button className={styles.continueButton}>Continuar</button>
+
+                    </footer>
+
+                </div>
+
+
+
+
             </div>
-            
-            <main className={styles.mainCart}>
-            <ProductInCart />
-            <ProductInCart />
-            <ProductInCart />
-            <ProductInCart />
-            <ProductInCart />
-            <ProductInCart />
-            </main> 
-            
-            
 
-            <footer className={styles.footerCart}>
-                <div className={styles.line}></div>
-
-                <div className={styles.totalPrice}>
-                    <p>Total:</p>
-                    <p>$ 30.000</p>
-                </div>
-
-                <button className={styles.continueButton}>Continuar</button>
-
-            </footer>
-
-        </div>
-
+            <div className={styles.versionDesk}> <HomeFooter /> </div>
+        </>
 
     );
 }
