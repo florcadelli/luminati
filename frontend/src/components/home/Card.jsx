3040eb6e7bfef0175d3409c7b8e8b4437c768c63
unknown <luciamilorodg@gmail.com>
2022-02-02 12:50:00 -0300

    usar el map para iterar el json en CardList/Main

--- frontend/src/components/home/Card.jsx
+++ frontend/src/components/home/Card.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import styles from "../../styles/home.module.css";
 import { FiShoppingCart } from "react-icons/fi";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-
-function Card() {
+function Card(props) {
   return (
     <>
       <div className={styles.containerCard}>
         <Link to="/detalleProducto">
-          <img src={require("../../img/iluminacion01.jpg")} />
+          
+          <img src={props.infoCard.img} />
           <div className={styles.img__Carrito}>
             <FiShoppingCart />
           </div>
-          <h3>Escritorio minimalista</h3>
-          <div><p>$12.00</p></div>
+          <h3>{props.infoCard.title}</h3>
+          <div>
+            <p>{props.infoCard.price} </p>
+          </div>
         </Link>
       </div>
-
-
     </>
   );
 }
