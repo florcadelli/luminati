9d65f756f14cc2ef11daa0bf06833c0882513882
sandra0411 <sandrapaludivan@gmail.com>
2022-02-09 16:42:01 -0300

    Creacion de Clases Java (Cart, User, Role, PaymentMethod) y mapeo de las mismas

---
+++ backend/luminati/src/main/java/com/example/luminati/model/Cart.java
@@ -0,0 +1,45 @@
+package com.example.luminati.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "Carts")
+@Getter
+@Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String status;
+
+    @ManyToMany
+    @JoinTable(name = "product_cart",
+            joinColumns = @JoinColumn(name = "id_cart"),
+            inverseJoinColumns = @JoinColumn(name = "id_product")
+    )
+    private Set<Product> products;
+
+    public Cart(){
+
+    }
+
+    public Cart(String status, Set<Product> products) {
+        this.status = status;
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "status='" + status + '\'' +
+                ", products=" + products +
+                '}';
+    }
+}
